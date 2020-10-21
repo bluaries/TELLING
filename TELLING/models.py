876e88037bb956d23b8138b9bf2641b1fe057f63
kidstylex <chilizhiki@gmail.com>
2020-10-21 16:33:24 +0700

    Create Model

--- TELLING/models.py
+++ TELLING/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
+class Category(models.Model):
+    category_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.category_name
+
+class Story(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+    
+    
