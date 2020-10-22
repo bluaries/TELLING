4a3887768eac2b48f5776d11d7613b69709443a0
mark47546 <mark47546natthaphon@gmail.com>
2020-10-22 13:33:35 +0700

    add user login function in views

--- TELLING/views.py
+++ TELLING/views.py
@@ -1,5 +1,33 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+# from login.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return HttpResponse("HI!!, You're at the TELLING index.")
+    return render(request, 'login/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in!")
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                if request.user.is_superuser:
+                    return redirect("admin:index")
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Failed to login.")
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, "login/login.html", {})
+
