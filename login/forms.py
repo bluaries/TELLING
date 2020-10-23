7cb00cf3eb95e743d194742bd0943e7979d83a48
mark47546 <mark47546natthaphon@gmail.com>
2020-10-23 17:09:17 +0700

    start app login

---
+++ login/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from login.models import UserProfileInfo
+from .models import AppUser
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = AppUser
+        fields = ('username','password','email')
