45e2213a6deeb8cac7a1759630729c5cf872829f
mark47546 <mark47546natthaphon@gmail.com>
2020-10-23 17:11:15 +0700

    move templates outside

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from TELLING import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TELLING.urls')),
+    path('admin/logout/', views.user_logout),
+    path('special/', views.special, name='special'),
+    path('login/', include('login.urls')),
+    path("logout/", views.user_logout, name="logout"),
 ]
