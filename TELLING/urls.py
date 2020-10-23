8de4b4c933a9648983164026e9c4f667cf1c4565
mark47546 <mark47546natthaphon@gmail.com>
2020-10-23 16:49:38 +0700

    add user_login path

--- TELLING/urls.py
+++ TELLING/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path('user_login/',views.user_login,name='user_login'),
 ]
