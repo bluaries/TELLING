5a4a598aff7f0d42f44293f9f58b936e367b41a8
mark47546 <mark47546natthaphon@gmail.com>
2020-10-26 10:20:09 +0700

    add user profile info class

--- login/forms.py
+++ login/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from login.models import UserProfileInfo
 from .models import AppUser
-
+from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta():
         model = AppUser
         fields = ('username','password','email')
+
+class UserProfileInfoForm(forms.ModelForm):
+     class Meta():
+         model = UserProfileInfo
+         fields = ('portfolio_site','profile_pic')
