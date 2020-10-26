8816ddd515cb53ec247fa0edfbb7dd718bd75ba1
mark47546 <mark47546natthaphon@gmail.com>
2020-10-26 10:22:11 +0700

    add register method

--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-# from login.forms import UserForm, UserProfileInfoForm
+from login.forms import UserForm, UserProfileInfoForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'login/index.html')
+    return render(request, 'login/index.html', {})
 
 @login_required
 def special(request):
@@ -18,6 +18,32 @@ def user_logout(request):
     logout(request)
     # return HttpResponseRedirect(reverse('index'))
     return redirect(reverse('login:user_login'))
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'profile_pic' in request.FILES:
+                print('found it')
+                profile.profile_pic = request.FILES['profile_pic']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+    return render(request,'login/registration.html',
+                          {'user_form':user_form,
+                           'profile_form':profile_form,
+                           'registered':registered})
 
 def user_login(request):
     if request.method == 'POST':
@@ -37,3 +63,4 @@ def user_login(request):
             return HttpResponse("Invalid login details given")
     else:
         return render(request, "login/login.html", {})
+
