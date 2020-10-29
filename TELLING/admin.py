774ab8df10879d711380be94dc2592fd5c1d07f8
kidstylex <chilizhiki@gmail.com>
2020-10-29 13:21:39 +0700

    Delete UserProfileInfo Model from TELLING app

--- TELLING/admin.py
+++ TELLING/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Author, Category, Story
-from login.models import UserProfileInfo, User
 
-admin.site.register(UserProfileInfo)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Story)
--- TELLING/models.py
+++ TELLING/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
 User = get_user_model()
 
@@ -29,10 +28,3 @@ class Story(models.Model):
 
     def __str__(self):
         return self.title
-# Login User Profile Info
-class UserProfileInfo(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    portfolio_site = models.URLField(blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
-    def __str__(self):
-        return self.user.username
