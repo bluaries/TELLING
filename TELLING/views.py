eda6de65cb83ea55ab4a34e416b3fc07525fbcf4
KOngTNP <tanapol.bu@ku.th>
2020-11-10 14:42:46 +0700

    add create story form

--- TELLING/views.py
+++ TELLING/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Story
+from .forms import StoryForm
 
 def index(request):
     queryset = Story.objects.filter()
@@ -7,3 +8,7 @@ def index(request):
         'Stories': queryset
     }
     return render(request, 'index.html', context)
+
+def create_story(request):
+    form = StoryForm()
+    return render(request, 'create_story.html', {'form': form})
