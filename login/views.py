2df262f8f7391f54f6c28a423cb514266bccc9f1
mark47546 <mark47546natthaphon@gmail.com>
2020-11-16 03:15:09 +0700

    commit

--- login/views.py
+++ login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from login.forms import UserForm, UserProfileInfoForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
