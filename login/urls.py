0c58632445732eae9b9458898a90ee14b77a6d16
kidstylex <chilizhiki@gmail.com>
2020-11-16 06:47:22 +0700

    Improve navigation

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,5 @@ app_name = "login"
 
 urlpatterns = [
     path('register/',views.register,name='register'),
-    path('user_login/',views.user_login,name='user_login'),
+    path('',views.user_login,name='user_login'),
 ]
