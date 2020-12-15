60c9988d78a1d21b32e340efff43988be4497eed
theethaj <bank30827@gmail.com>
2020-12-16 00:02:45 +0700

    Model tests completed

--- TELLING/tests/test_models/test_category_model.py
+++ TELLING/tests/test_models/test_category_model.py
@@ -1,17 +1,22 @@
 from django.test import TestCase
 from TELLING import models
-from TELLING.models import Author, Category, Story
+from TELLING.models import Category, Story
+from django.contrib.auth.models import User
 
 
 class CategoryModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        Category.objects.create(category_name="Love")
+    def setUp(self):
+        user = User.objects.create_user("User 1", password="pws")
+        category = Category.objects.create(category_name="Category 1")
+        Story.objects.create(
+            title="Story 1", author=user,
+            categories=category, thumbnail='upload/Grass_Railways.jpg'
+        )
 
     def test_category_max_length(self):
         """
         Test max length of category.
         """
-        category = Category.objects.get(id=1)
+        category = Category.objects.get(pk=1)
         category_max_length = category._meta.get_field('category_name').max_length
-        self.assertEquals(category_max_length, 200)
+        self.assertEquals(category_max_length, 50)
--- TELLING/tests/test_models/test_story_model.py
+++ TELLING/tests/test_models/test_story_model.py
@@ -1,19 +1,22 @@
 from django.test import TestCase
 from TELLING import models
-from TELLING.models import Author, Category, Story
+from TELLING.models import Category, Story
+from django.contrib.auth.models import User
 
 
 class StoryModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        author = Author.objects.create(user='Jay')
-        category = Category.objects.create(category_name="Love")
-        Story.objects.create(author=author, category=category, title="Cars555")
+    def setUp(self):
+        user = User.objects.create_user("User 1", password="pws")
+        category = Category.objects.create(category_name="Category 1")
+        Story.objects.create(
+            title="Story 1", author=user,
+            categories=category, thumbnail='upload/Grass_Railways.jpg'
+        )
 
     def test_story_max_length(self):
         """
         Test max length of story.
         """
-        story = Story.objects.get(id=1)
+        story = Story.objects.get(pk=1)
         story_max_length = story._meta.get_field('title').max_length
-        self.assertEquals(story_max_length, 200)
+        self.assertEquals(story_max_length, 100)
